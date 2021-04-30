986df1337a4fa616dbbbd3eb46353eacebd6e9bd
Sumon Biswas <50983252+sumonbiswas2010@users.noreply.github.com>
2021-04-30 23:45:58 +0600

    Update app.js

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ app.use(express.json());
 app.use("/api/users", userRouter);
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => {
+app.listen(port || 3000, () => {
   console.log("server up and running on PORT :", port);
 });
