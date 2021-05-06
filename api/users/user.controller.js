8cf93bf4602125e1d3ea25efe57e71915fc53323
Sumon Biswas <sumonbiswas2010@gmail.com>
2021-05-06 18:30:34 +0600

    Update user.controller.js

--- api/users/user.controller.js
+++ api/users/user.controller.js
@@ -16,30 +16,32 @@ module.exports = {
   createUser: (req, res) => {
     const body = req.body;
     //console.log("Starting" +body);
-    //const body = {
-     //first_name: "Sumon",
-      //last_name: "Biswas",
-      //gender: "Male",
-      //email: "tetsdgfg@dg.dfcom",
-      //password:"ok",
-      //number: "067123"
-  //};
-
-    var salt = bcrypt.genSaltSync(10);
-    body.password = bcrypt.hashSync(body.password, salt);
-    create(body, (err, results) => {
-      if (err) {
-        console.log(err);
-        return res.status(500).json({
-          success: 0,
-          message: "Database connection errror"
+    getUserByUserEmail(body.email, (err, results) => {
+      if(results)
+      {
+        res.status(400).send();
+      }
+      else
+      {
+        var salt = bcrypt.genSaltSync(10);
+        body.password = bcrypt.hashSync(body.password, salt);
+        create(body, (err, results) => {
+          if (err) {
+            console.log(err);
+            return res.status(500).json({
+              success: 0,
+              message: "Database connection errror"
+            });
+          }
+          return res.status(200).json({
+            success: 1,
+            data: results
+          });
         });
       }
-      return res.status(200).json({
-        success: 1,
-        data: results
-      });
     });
+
+
   },
   login: (req, res) => {
     const body = req.body;
